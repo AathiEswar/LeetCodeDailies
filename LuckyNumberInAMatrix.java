e5511d94fb1f0366a7e25729680417b71637dd93
AathiEswar <aathi28082003@gmail.com>
2024-07-19 10:33:48 +0530

    19-7-2024

--- LuckyNumberInAMatrix.java
+++ LuckyNumberInAMatrix.java
@@ -1,6 +1,47 @@
 package LeetCodeDaily;
 import java.util.*;
 public class LuckyNumberInAMatrix {
+    public List<Integer> luckyNumberOptimize(int[][] matrix){
+        List<Integer> result = new ArrayList<>();
+
+
+        for(int i = 0; i<matrix.length;i++){
+
+            int rowMinIndex  = findMinInRow(matrix,i);
+            int colMaxVal = matrix[i][rowMinIndex];
+
+            if(isMaxInCol(matrix , colMaxVal , rowMinIndex)){
+                result.add(colMaxVal);
+            }
+        }
+
+        return result;
+    }
+
+    public int findMinInRow(int[][] matrix , int row){
+        int min = matrix[row][0] ;
+        int rowIndex = 0;
+
+        for(int i = 0 ;i<matrix[0].length;i++){
+            if(min > matrix[row][i]){
+                min = matrix[row][i];
+                rowIndex = i;
+            }
+        }
+
+        return rowIndex;
+    }
+
+    public boolean isMaxInCol(int[][] matrix , int maxVal , int maxIndex  ){
+
+        for (int i = 0; i < matrix.length; i++) {
+            if(maxVal < matrix[i][maxIndex]){
+                return false;
+            }
+        }
+
+        return true;
+    }
     public List<Integer> luckyNumbersOwn (int[][] matrix) {
         ArrayList<Integer> minList = new ArrayList<>();
         ArrayList<Integer> maxList = new ArrayList<>();
