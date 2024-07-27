27d8c77b086c59238dedca90719b062f4813f103
AathiEswar <aathi28082003@gmail.com>
2024-07-27 08:20:11 +0530

    27-7-2024

--- FindCitySmallestThreshold.java
+++ FindCitySmallestThreshold.java
@@ -1,6 +1,57 @@
 package LeetCodeDaily;
 import java.util.*;
 public class FindCitySmallestThreshold {
+    class FloydsWarshellAlgo{
+        public int findTheCity(int n, int[][] edges, int k) {
+
+            int[][] graph  = new int[n][n];
+
+            for(int i = 0 ; i<n ;i++){
+                Arrays.fill( graph[i] , Integer.MAX_VALUE) ;
+                graph[i][i] = 0;
+            }
+
+            for(int[] edge : edges){
+                int source = edge[0];
+                int dest   = edge[1];
+                int cost   = edge[2];
+
+                graph[source][dest] = cost;
+                graph[dest][source] = cost;
+            }
+
+            for(int mid = 0 ; mid < n ; mid++){
+                for(int source = 0 ; source < n ; source++){
+                    for(int dest = 0 ; dest < n  ; dest++ ){
+                        if(graph[source][mid] < Integer.MAX_VALUE && graph[mid][dest] < Integer.MAX_VALUE){
+                            graph[source][dest] =
+                                    Math.min(graph[source][dest] , graph[source][mid] + graph[mid][dest] );
+                        }
+                    }
+                }
+            }
+
+            int city = 0 ;
+            int min = n;
+
+            for(int source = 0 ; source < n ; source++){
+                int count = 0;
+
+                for(int dest = 0 ; dest < n ; dest++){
+                    if(graph[source][dest] <= k){
+                        count++;
+                    }
+                }
+
+                if(count<=min){
+                    city = source;
+                    min = count;
+                }
+            }
+
+            return city ;
+        }
+    }
     public int findTheCity(int n, int[][] edges, int k) {
         // create adjList
         // traverse through every node and find the max dis it can go
